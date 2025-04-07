f06526cc213e7ee0595b69ff2f5f9367aaa0ad52
rantokl <lalainazonomentsoa@gmail.com>
2025-04-07 15:10:58 +0200

    [ADD]

---
+++ motors.py
@@ -0,0 +1,75 @@
+import RPi.GPIO as GPIO
+import time
+
+# Configuration des broches PWM pour les moteurs
+PWM_PINS = [12, 13]  # Moteurs 1 à 4 18, 19
+DIR_PINS = [  # 2 broches direction par moteur
+    (20, 21),  # Moteur 1
+    (16, 26),  # Moteur 2
+    #(19, 17),  # Moteur 3
+    #(4, 25)    # Moteur 4
+]
+FREQUENCY = 1000  # en Hz
+
+# Initialisation GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Configuration PWM et direction
+for pin in PWM_PINS:
+    GPIO.setup(pin, GPIO.OUT)
+
+for dir1, dir2 in DIR_PINS:
+    GPIO.setup(dir1, GPIO.OUT)
+    GPIO.setup(dir2, GPIO.OUT)
+
+# Création des PWM
+pwms = [GPIO.PWM(pin, FREQUENCY) for pin in PWM_PINS]
+
+# Démarrer les PWM
+for pwm in pwms:
+    pwm.start(0)
+
+def set_motor(motor_index, speed, direction):
+    """
+    motor_index: 0 à 3
+    speed: 0 à 100 (duty cycle)
+    direction: 'forward' ou 'backward'
+    """
+    dir1, dir2 = DIR_PINS[motor_index]
+
+    if direction == 'forward':
+        GPIO.output(dir1, GPIO.HIGH)
+        GPIO.output(dir2, GPIO.LOW)
+    elif direction == 'backward':
+        GPIO.output(dir1, GPIO.LOW)
+        GPIO.output(dir2, GPIO.HIGH)
+    else:
+        GPIO.output(dir1, GPIO.LOW)
+        GPIO.output(dir2, GPIO.LOW)
+
+    pwms[motor_index].ChangeDutyCycle(speed)
+
+# try:
+#     while True:
+#         print("Tous les moteurs en avant")
+#         for i in range(2):
+#             set_motor(i, 70, 'forward')  # 70% de vitesse
+#         time.sleep(5)
+
+#         print("Tous les moteurs en arrière")
+#         for i in range(2):
+#             set_motor(i, 50, 'backward')  # 50% de vitesse
+#         time.sleep(5)
+
+#         print("Arrêt")
+#         for i in range(2):
+#             set_motor(i, 0, 'stop')
+#         time.sleep(5)
+
+# except KeyboardInterrupt:
+#     print("Arrêt du programme")
+
+# finally:
+#     for pwm in pwms:
+#         pwm.stop()
+#     GPIO.cleanup()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import threading
 import time
 import sensors
+import motors
 
 app = Flask(__name__)
 
@@ -14,6 +15,14 @@ def update_distances():
         distances = sensors.get_all_distances()
         time.sleep(1)
 
+def run_motors():
+    while True:
+        if axis == 'x':
+            motors.set_motor(1,value,'forward')
+            motors.set_motor(2,value,'forward')
+        elif axis == 'y':
+            motors.set_motor(1,value,'backward')
+            motors.set_motor(1,value,'backward')
 
 @app.route('/')
 def index():
@@ -26,13 +35,20 @@ def get_distances():
 @app.route('/send', methods=['POST'])
 def receive_message():
     data = request.get_json()
-    message = data.get('message')
+    message = data.get('message', {})
+    global axis, value 
+    axis = message.get('axis')
+    value = message.get('value')
     print(f"Message reçu depuis AJAX : {message}")
+
     return jsonify({"status": "ok", "message": message})
+
+    
 
 if __name__ == '__main__':
     try:
         thread = threading.Thread(target=update_distances)
+        thread = threading.Thread(target=run_motors)
         thread.daemon = True
         thread.start()
         app.run(host='0.0.0.0', port=8089, debug=True)
