c863a7739b3ab1862d28e01a91f3a8377ccc3eeb
rantokl <lalainazonomentsoa@gmail.com>
2025-04-08 23:19:24 +0200

    [ADD]

--- motors.py
+++ motors.py
@@ -1,44 +1,43 @@
 from roboclaw_3 import Roboclaw
 from time import sleep
 
-class MotorController:
-    def __init__(self):
-        self.moteur12 = Roboclaw("/dev/ttyACM0", 38400)
-        self.moteur34 = Roboclaw("/dev/ttyS0", 38400)
-        self.address = 0x80
-        self.moteur12.Open()
-        self.moteur34.Open()
 
-    def marche_avant(self,vitesse):
-        self.moteur12.ForwardM1(self.address,vitesse)
-        self.moteur34.ForwardM1(self.address,vitesse)
+moteur12 = Roboclaw("/dev/ttyACM0", 38400)
+moteur34 = Roboclaw("/dev/ttyS0", 38400)
+address = 0x80
+moteur12.Open()
+moteur34.Open()
 
-        self.moteur12.ForwardM2(self.address,vitesse)
-        self.moteur34.ForwardM2(self.address,vitesse)
-  
+def marche_avant(vitesse):
+    moteur12.ForwardM1(address,vitesse)
+    moteur34.ForwardM1(address,vitesse)
 
-    def marche_arriere(self,vitesse):
-        self.moteur12.BackwardM1(self.address,vitesse)
-        self.moteur34.BackwardM1(self.address,vitesse)
-
-        self.moteur12.BackwardM2(self.address,vitesse)
-        self.moteur34.BackwardM2(self.address,vitesse)
+    moteur12.ForwardM2(address,vitesse)
+    moteur34.ForwardM2(address,vitesse)
 
 
-    def tourner_gauche(self,vitesse):
-        self.moteur12.BackwardM2(self.address,vitesse)
-        self.moteur34.BackwardM1(self.address,vitesse)
+def marche_arriere(vitesse):
+    moteur12.BackwardM1(address,vitesse)
+    moteur34.BackwardM1(address,vitesse)
 
-    def tourner_droite(self,vitesse):
-        self.moteur12.BackwardM1(self.address,vitesse)
-        self.moteur34.BackwardM2(self.address,vitesse)
+    moteur12.BackwardM2(address,vitesse)
+    moteur34.BackwardM2(address,vitesse)
 
-    def stop(self):
-        self.moteur12.ForwardM1(self.address,0)
-        self.moteur34.ForwardM1(self.address,0)
 
-        self.moteur12.ForwardM2(self.address,0)
-        self.moteur34.ForwardM2(self.address,0)
+def tourner_gauche(vitesse):
+    moteur12.BackwardM2(address,vitesse)
+    moteur34.BackwardM1(address,vitesse)
+
+def tourner_droite(self,vitesse):
+    moteur12.BackwardM1(address,vitesse)
+    moteur34.BackwardM2(address,vitesse)
+
+def stop(self):
+    moteur12.ForwardM1(address,0)
+    moteur34.ForwardM1(address,0)
+
+    moteur12.ForwardM2(address,0)
+    moteur34.ForwardM2(address,0)
 
         
     
--- packet_serial_bus.py
+++ packet_serial_bus.py
@@ -1,19 +1,17 @@
 from roboclaw_3 import Roboclaw
 from time import sleep
+from motors import *
 
-from motors import MotorController
 
-motor = MotorController()
-
-motor.marche_avant(32)
+marche_avant(32)
 sleep(2)
-motor.tourner_droite(32)
+tourner_droite(32)
 sleep(2)
-motor.tourner_gauche(32)
+tourner_gauche(32)
 sleep(2)
-motor.marche_arriere(32)
+marche_arriere(32)
 sleep(2)
-motor.stop()
+stop()
 sleep(2)
     
 
