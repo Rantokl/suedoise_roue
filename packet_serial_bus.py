62e6835e1a5a709fc1422ddb9f364a2101caf3e9
rantokl <lalainazonomentsoa@gmail.com>
2025-04-08 22:01:04 +0200

    [ADD]

--- packet_serial_bus.py
+++ packet_serial_bus.py
@@ -1,30 +1,30 @@
 from roboclaw_3 import Roboclaw
 from time import sleep
 
-addresses = [0x80,0x81]
-roboclaw = Roboclaw("/dev/ttyS0", 38400)
+address = 0x80
+roboclaw = Roboclaw("/dev/ttyACM0", 38400)
 roboclaw.Open()
 
 while True:
     
-    for address in addresses:
+    
         
-        roboclaw.ForwardM1(address,32)
-        roboclaw.ForwardM2(address,32)
-        #roboclaw.ForwardM2(address,64)
-        sleep(2)
-        roboclaw.ForwardM1(address,0)
-        roboclaw.ForwardM2(address,0)
-        #roboclaw.ForwardM2(address,0)
-        sleep(2)
-        roboclaw.BackwardM1(address,32)
-        roboclaw.BackwardM2(address,32)
-        #roboclaw.ForwardM2(address,64)
-        sleep(2)
-        roboclaw.ForwardM1(address,0)
-        roboclaw.ForwardM2(address,0)
-        #roboclaw.ForwardM2(address,0)
-        sleep(2)
+    roboclaw.ForwardM1(address,32)
+    roboclaw.ForwardM2(address,32)
+    #roboclaw.ForwardM2(address,64)
+    sleep(2)
+    roboclaw.ForwardM1(address,0)
+    roboclaw.ForwardM2(address,0)
+    #roboclaw.ForwardM2(address,0)
+    sleep(2)
+    roboclaw.BackwardM1(address,32)
+    roboclaw.BackwardM2(address,32)
+    #roboclaw.ForwardM2(address,64)
+    sleep(2)
+    roboclaw.ForwardM1(address,0)
+    roboclaw.ForwardM2(address,0)
+    #roboclaw.ForwardM2(address,0)
+    sleep(2)
         
     
 
