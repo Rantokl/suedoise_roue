38fae7ad32b9f4b18c6babccc6fb78ff95c64101
rantokl <lalainazonomentsoa@gmail.com>
2025-04-08 23:39:35 +0200

    [ADD]

--- packet_serial_bus.py
+++ packet_serial_bus.py
@@ -3,16 +3,16 @@ from time import sleep
 from motors import *
 
 
-marche_avant(24)
+marche_avant(32)
 stop()
 sleep(0.5)
-tourner_droite(24)
+tourner_droite(32)
 stop()
 sleep(0.5)
-tourner_gauche(24)
+tourner_gauche(32)
 stop()
 sleep(0.5)
-marche_arriere(24)
+marche_arriere(32)
 sleep(2)
 stop()
 
