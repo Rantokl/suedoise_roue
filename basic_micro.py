a538b0f77170eb1024fd2b8e988d0186ca1472fa
rantokl <lalainazonomentsoa@gmail.com>
2025-04-08 13:40:35 +0200

    [ADD]

---
+++ basic_micro.py
@@ -0,0 +1,21 @@
+import time
+from roboclaw_3 import Roboclaw
+
+#Windows comport name
+rc = Roboclaw("/dev/ttyS0",38400)
+#Linux comport name
+#rc = Roboclaw("/dev/ttyACM0",115200)
+
+if(rc):
+	print("Roboclaw connected")
+
+rc.Open()
+address = 0x80
+
+
+for i in range(0,32):
+    rc.ForwardM1(address,i*2)
+    time.sleep(1)	#1/4 power forward
+	# rc.BackwardM2(address,64)
+	# print("Backward")	#1/4 power backward
+	
