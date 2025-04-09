22f45cec2f48c5042b3e8a2939fbd30cf6ffa383
rantokl <lalainazonomentsoa@gmail.com>
2025-04-09 16:15:42 +0200

    [ADD]

--- motors.py
+++ motors.py
@@ -9,27 +9,27 @@ moteur12.Open()
 moteur34.Open()
 
 def marche_avant(vitesse):
-    #moteur12.BackwardM1(address,vitesse) 
-    #moteur12.ForwardM2(address,vitesse)
+    moteur12.BackwardM1(address,vitesse) 
+    moteur12.ForwardM2(address,vitesse)
     moteur34.ForwardM2(address,vitesse)
     moteur34.BackwardM1(address,vitesse)
     
 def marche_arriere(vitesse):
     moteur34.ForwardM1(address,vitesse)
     moteur34.BackwardM2(address,vitesse)
-    #moteur12.BackwardM2(address,vitesse)
-    #moteur12.ForwardM1(address,vitesse)
+    moteur12.BackwardM2(address,vitesse)
+    moteur12.ForwardM1(address,vitesse)
     
     
 def tourner_gauche(vitesse):
-    #moteur12.ForwardM1(address,int(vitesse/2))
-    #moteur12.ForwardM2(address,vitesse)
+    moteur12.ForwardM1(address,int(vitesse/2))
+    moteur12.ForwardM2(address,vitesse)
     moteur34.BackwardM2(address,int(vitesse/2))
     moteur34.BackwardM1(address,vitesse)
 
 def tourner_droite(vitesse):
-    #moteur12.BackwardM1(address,vitesse) 
-    #moteur12.BackwardM2(address,int(vitesse/2))
+    moteur12.BackwardM1(address,vitesse) 
+    moteur12.BackwardM2(address,int(vitesse/2))
     moteur34.ForwardM2(address,vitesse)
     moteur34.ForwardM1(address,int(vitesse/2))
 
