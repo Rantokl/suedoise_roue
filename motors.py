2a70f018aafa1a4625adfc06d43752a740af401d
rantokl <lalainazonomentsoa@gmail.com>
2025-04-09 15:57:23 +0200

    [ADD]

--- motors.py
+++ motors.py
@@ -2,42 +2,43 @@ from roboclaw_3 import Roboclaw
 from time import sleep
 
 
-moteur12 = Roboclaw("/dev/ttyACM0", 115200)
+#moteur34 = Roboclaw("/dev/ttyACM0", 115200)
 moteur34 = Roboclaw("/dev/ttyS0", 115200)
-address = 0x80
-moteur12.Open()
+address1 = 0x80
+address2 = 0x81
+#moteur34.Open()
 moteur34.Open()
 
 def marche_avant(vitesse):
-    moteur12.BackwardM1(address,vitesse) 
-    moteur12.ForwardM2(address,vitesse)
-    moteur34.ForwardM2(address,vitesse)
-    moteur34.BackwardM1(address,vitesse)
+    moteur34.BackwardM1(address1,vitesse) 
+    moteur34.ForwardM2(address2,vitesse)
+    moteur34.ForwardM2(address1,vitesse)
+    moteur34.BackwardM1(address2,vitesse)
     
 def marche_arriere(vitesse):
-    moteur34.ForwardM1(address,vitesse)
-    moteur34.BackwardM2(address,vitesse)
-    moteur12.BackwardM2(address,vitesse)
-    moteur12.ForwardM1(address,vitesse)
+    moteur34.ForwardM1(address1,vitesse)
+    moteur34.BackwardM2(address2,vitesse)
+    moteur34.BackwardM2(address1,vitesse)
+    moteur34.ForwardM1(address2,vitesse)
     
     
 def tourner_gauche(vitesse):
-    moteur12.ForwardM1(address,int(vitesse/2))
-    moteur12.ForwardM2(address,vitesse)
-    moteur34.BackwardM2(address,int(vitesse/2))
-    moteur34.BackwardM1(address,vitesse)
+    moteur34.ForwardM1(address1,int(vitesse/2))
+    moteur34.ForwardM2(address2,vitesse)
+    moteur34.BackwardM2(address1,int(vitesse/2))
+    moteur34.BackwardM1(address2,vitesse)
 
 def tourner_droite(vitesse):
-    moteur12.BackwardM1(address,vitesse) 
-    moteur12.BackwardM2(address,int(vitesse/2))
-    moteur34.ForwardM2(address,vitesse)
-    moteur34.ForwardM1(address,int(vitesse/2))
+    moteur34.BackwardM1(address1,vitesse) 
+    moteur34.BackwardM2(address2,int(vitesse/2))
+    moteur34.ForwardM2(address1,vitesse)
+    moteur34.ForwardM1(address2,int(vitesse/2))
 
 def stop(): 
-    moteur12.ForwardM1(address,0)
-    moteur12.ForwardM2(address,0)
-    moteur34.ForwardM2(address,0)
-    moteur34.ForwardM1(address,0)
+    moteur34.ForwardM1(address1,0)
+    moteur34.ForwardM2(address2,0)
+    moteur34.ForwardM2(address1,0)
+    moteur34.ForwardM1(address2,0)
     
 
 
