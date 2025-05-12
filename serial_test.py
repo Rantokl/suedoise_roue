6b32870386d8b11314add75a83a2148d3dfa521b
rantokl <lalainazonomentsoa@gmail.com>
2025-05-12 10:52:27 +0200

    [ADD]

---
+++ serial_test.py
@@ -0,0 +1,10 @@
+import serial
+
+# Remplace ttyACM0 par le port correspondant si besoin
+ser = serial.Serial('/dev/ttyACM0', 115200)
+
+print("Lecture depuis le micro:bit...")
+while True:
+    if ser.in_waiting:
+        data = ser.readline().decode('utf-8').strip()
+        print("Reçu :", data)
