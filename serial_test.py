f16a9249e31eb9f6da5654d065db5aa00aadfd5b
rantokl <lalainazonomentsoa@gmail.com>
2025-05-19 13:50:56 +0200

    [ADD]

--- serial_test.py
+++ serial_test.py
@@ -1,7 +1,7 @@
 import serial
 
 # Remplace ttyACM0 par le port correspondant si besoin
-ser = serial.Serial('/dev/ttyACM1', 115200)
+ser = serial.Serial('/dev/ttyACM0', 115200)
 
 print("Lecture depuis le micro:bit...")
 while True:
