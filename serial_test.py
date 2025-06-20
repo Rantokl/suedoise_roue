bcf451e3d110fc353484232b7dabe63d6a93d30a
rantokl <lalainazonomentsoa@gmail.com>
2025-06-20 09:14:37 +0200

    [ADD]

--- serial_test.py
+++ serial_test.py
@@ -1,6 +1,46 @@
 import serial
 
 # Remplace ttyACM0 par le port correspondant si besoin
+def joystick_to_scale(value):
+    if 570 <= value <= 590:
+        return 0  # Zone morte
+    elif value > 590:
+        return int((value - 590) * 64 / (1023 - 590))
+    elif value < 570:
+        return int((570 - value) * 64 / 570)
+
+def get_direction(x, y):
+    dx = joystick_to_scale(x)
+    dy = joystick_to_scale(y)
+
+    direction = "repos"
+
+    if dx == 0 and dy == 0:
+        direction = "repos"
+    elif dx > 0 and dy == 0:
+        direction = "droite"
+    elif dx < 0 and dy == 0:
+        direction = "gauche"
+    elif dx == 0 and dy > 0:
+        direction = "haut"
+    elif dx == 0 and dy < 0:
+        direction = "bas"
+    elif dx > 0 and dy > 0:
+        direction = "haut-droite"
+    elif dx < 0 and dy > 0:
+        direction = "haut-gauche"
+    elif dx > 0 and dy < 0:
+        direction = "bas-droite"
+    elif dx < 0 and dy < 0:
+        direction = "bas-gauche"
+
+    print(f"Valeurs converties : X = {dx}, Y = {dy} -> Direction : {direction}")
+    return dx, dy, direction
+
+
+
+
+
 ser = serial.Serial('/dev/ttyACM1', 115200)
 
 print("Lecture depuis le micro:bit...")
